8487d9686c1ba828979c199c73b60155da440fa4
SFNTRmain <133243192+SFNTRmain@users.noreply.github.com>
2023-09-23 20:23:17 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://moviewebapp.ssbuem67.workers.dev",
+  VITE_TMDB_READ_API_KEY: "6f739dc00f165fcc852d0e73ddcf0e01"
+};
