c3a1d1ca77f078f6cc4173d54fca1e7622f82af0
SFNTRmain <133243192+SFNTRmain@users.noreply.github.com>
2023-09-23 20:29:09 +0200

    Update config.js
    read api key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://moviewebapp.ssbuem67.workers.dev",
-  VITE_TMDB_READ_API_KEY: "6f739dc00f165fcc852d0e73ddcf0e01"
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZjczOWRjMDBmMTY1ZmNjODUyZDBlNzNkZGNmMGUwMSIsInN1YiI6IjY1MGYyM2M4ZThkMGI0MDE0ZTBkYmFiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.g7OaGlZ1pNWjdFYB-nvhzYTfR5TQiUwDyIAOUAR7f8s"
 };
